a709972745a3f3afec86144df3ceeed1a7b8f345
siphokazi-codex <aplenis@webmail.co.za>
2017-06-14 14:07:04 +0200

    Added modal windows and handledbars template on my indedx.html and css file

--- hackathon.js
+++ hackathon.js
@@ -1,5 +1,46 @@
-var addBtn = getElementsByName('addBtn');
-var findBtn = getElementsByName('findBtn');
-var searchBtn = getElementsByName('searchBtn');
-var displayBtn = getElementsByName('displayBtn');
-var addText = document.querySelector('addText');
+//Declaring my buttons
+
+var searchBtn = document.querySelector('#searchBtn');
+
+//Display data on program load
+var searchTempText = document.querySelector('.searchTemp').innerHTML;
+var customerTempText = document.querySelector('.customerTemp').innerHTML;
+
+//for template compile
+var searchTemplate = Handlebars.compile(searchTempText);
+var customerTemplate = Handlebars.compile(customerTempText);
+
+//targets the div on the html file
+var addListElem = document.querySelector('#add');
+var displayListElem = document.querySelector('#display');
+var findListElem = document.querySelector('#find');
+var searchListElem = document.querySelector('#search');
+
+//elements for the modal box
+var findModal = document.querySelector('#findModal');
+var detailsModal = document.querySelector('#detailsModal');
+var addModal = document.querySelector('#addModal');
+
+// Get the button that opens the modal
+var AddBtn = document.querySelector("#addBtn");
+var FindBtn = document.querySelector("#findBtn");
+var DisplayBtn = document.querySelector("#displayBtn");
+
+// Get the <span> element that closes the modal
+var span = document.querySelector(".close")[0];
+
+
+// When the user clicks on the button, open the modal
+AddBtn.onclick = function() {
+    addModal.style.display = "block";
+}
+
+// When the user clicks on the button, open the modal
+FindBtn.onclick = function() {
+    findModal.style.display = "block";
+}
+
+// When the user clicks on the button, open the modal
+DisplayBtn.onclick = function() {
+    detailsModal.style.display = "block";
+}
