1bd5c8af54ae3c624445939e112a5e58637270f7
bootcamp <makhosandilezukelwa@gmail.com>
2017-06-14 12:06:12 +0200

    added an Add model

---
+++ hackathon.js
@@ -0,0 +1,10 @@
+var addBtn = getElementsByName('addBtn');
+var findBtn = getElementsByName('findBtn');
+var searchBtn = getElementsByName('searchBtn');
+var displayBtn = getElementsByName('displayBtn');
+var addText = document.querySelector('addText');
+
+addText.addEventlistener("", function(){
+
+
+}
